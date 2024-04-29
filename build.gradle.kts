84d6708e8f9d172da3b952818bb17a2d35e7324e
Thomas Kolmans <thomas@kolmans.com>
2024-04-29 23:22:21 +0200

    new release structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,21 @@
 plugins {
     kotlin("jvm") version "1.9.23"
-    `maven-publish`
-    signing
-    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 object Meta {
+    const val name = "gsjson"
     const val desc = "GSJson is a getter/setter syntax interpretation language"
     const val license = "Apache-2.0"
     const val githubRepo = "transportial/gsjson"
-    const val release = "https://s01.oss.sonatype.org/service/local/"
-    const val snapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
 }
 
 group = "com.transportial"
 version = "1.0.4"
 
-val repositories = arrayOf(
-    "https://oss.sonatype.org/content/repositories/snapshots/",
-    "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-)
-
 repositories {
     mavenLocal()
     mavenCentral()
-    repositories.forEach { maven(it) }
 }
 
 dependencies {
@@ -42,86 +33,37 @@ kotlin {
     jvmToolchain(17)
 }
 
-val sourcesJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(kotlin.sourceSets.main.get().kotlin)
-}
-val javadocJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("javadoc")
-    from(kotlin.sourceSets.main.get().kotlin)
-}
 
-artifacts {
-    archives(sourcesJar)
-    archives(javadocJar)
-}
+mavenPublishing {
+    coordinates(group.toString(), Meta.name, version.toString())
 
-signing {
-    val signingKey = providers
-        .environmentVariable("GPG_SIGNING_KEY")
-    val signingPassphrase = providers
-        .environmentVariable("GPG_SIGNING_PASSPHRASE")
-
-    if (signingKey.isPresent && signingPassphrase.isPresent) {
-        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
-        val extension = extensions
-            .getByName("publishing") as PublishingExtension
-        sign(extension.publications)
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-            from(components["kotlin"])
-            artifact(tasks["sourcesJar"])
-            artifact(tasks["javadocJar"])
-            pom {
-                name.set(project.name)
-                description.set(Meta.desc)
-                url.set("https://github.com/${Meta.githubRepo}")
-                licenses {
-                    license {
-                        name.set(Meta.license)
-                        url.set("https://opensource.org/licenses/Apache-2.0")
-                    }
-                }
-                developers {
-                    developer {
-                        id = "thomaskolmans"
-                        name = "Thomas Kolmans"
-                        email = "thomas.kolmans@transportial.com"
-                    }
-                }
-                scm {
-                    url.set("https://github.com/${Meta.githubRepo}.git")
-                    connection.set("scm:git:git://github.com/${Meta.githubRepo}.git")
-                    developerConnection.set("scm:git:git://github.com/#${Meta.githubRepo}.git")
-                }
-                issueManagement {
-                    url.set("https://github.com/${Meta.githubRepo}/issues")
-                }
+    pom {
+        name.set(Meta.name)
+        description.set(Meta.desc)
+        inceptionYear.set("2024")
+        url.set("https://github.com/${Meta.githubRepo}")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
         }
-    }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri(Meta.release))
-            snapshotRepositoryUrl.set(uri(Meta.snapshot))
-            val ossrhUsername = providers
-                .environmentVariable("OSSRH_USERNAME")
-            val ossrhPassword = providers
-                .environmentVariable("OSSRH_PASSWORD")
-            if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
-                username.set(ossrhUsername.get())
-                password.set(ossrhPassword.get())
+        organization {
+            name = "Transportial BV"
+            url = "https://transportial.com"
+        }
+        developers {
+            developer {
+                id.set("thomaskolmans")
+                name.set("Thomas Kolmans")
+                url.set("https://github.com/thomaskolmans/")
             }
+        }
+        scm {
+            url.set("https://github.com/${Meta.githubRepo}")
+            connection.set("scm:git:git://github.com/${Meta.githubRepo}.git")
+            developerConnection.set("scm:git:ssh://git@github.com/${Meta.githubRepo}.git")
         }
     }
 }
