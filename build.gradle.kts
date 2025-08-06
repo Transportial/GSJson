73bde4b9eecc3907e3e25d99b9391c69eb1dbd29
Talha <talha.akbar@n0name.eu>
2025-08-06 12:25:08 +0500

    misc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
+import java.util.Base64
 
 plugins {
     kotlin("jvm") version "1.9.23"
-//    id("com.vanniktech.maven.publish") version "0.28.0"
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
@@ -34,8 +34,44 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+// Add this task for automatic release
+tasks.register("publishAndAutoRelease") {
+    group = "publishing"
+    description = "Publishes to Sonatype and automatically releases"
+
+    dependsOn("publishToSonatype")
+
+    doLast {
+        val username = findProperty("centralPortalUsername") as String?
+        val password = findProperty("centralPortalPassword") as String?
+
+        if (username != null && password != null) {
+            val credentials = Base64.getEncoder().encodeToString("$username:$password".toByteArray())
+
+            exec {
+                commandLine("curl", "-X", "POST",
+                    "-H", "Authorization: Bearer $credentials",
+                    "https://ossrh-staging-api.central.sonatype.com/manual/upload/defaultRepository/${project.group}?publishing_type=automatic"
+                )
+            }
+            println("Automatic release initiated for ${project.group}:${project.name}:${project.version}")
+        } else {
+            println("Credentials not found. Please check your gradle.properties file.")
+        }
+    }
+}
+
 kotlin {
     jvmToolchain(17)
+}
+
+// Required for Maven Central - generate sources and javadoc JARs
+java {
+    withSourcesJar()
+    withJavadocJar()
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 publishing {
@@ -44,29 +80,35 @@ publishing {
             from(components["java"])
 
             pom {
-                name.set("Your Project Name")
-                description.set("A brief description of your project")
-                url.set("https://github.com/yourusername/yourproject")
+                name.set(Meta.name)
+                description.set(Meta.desc)
+                url.set("https://github.com/${Meta.githubRepo}")
 
                 licenses {
                     license {
                         name.set("The Apache License, Version 2.0")
                         url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                     }
+                }
+
+                organization {
+                    name.set("Transportial BV")
+                    url.set("https://transportial.com")
                 }
 
                 developers {
                     developer {
-                        id.set("yourusername")
-                        name.set("Your Name")
-                        email.set("your.email@example.com")
+                        id.set("thomaskolmans")
+                        name.set("Thomas Kolmans")
+                        url.set("https://github.com/thomaskolmans/")
                     }
                 }
 
                 scm {
-                    connection.set("scm:git:git://github.com/transportial/gsjson.git")
-                    developerConnection.set("scm:git:ssh://github.com/transportial/gsjson.git")
-                    url.set("https://github.com/transportial/gsjson")
+                    connection.set("scm:git:git://github.com/${Meta.githubRepo}.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/${Meta.githubRepo}.git")
+                    url.set("https://github.com/${Meta.githubRepo}")
                 }
             }
         }
@@ -93,58 +135,14 @@ signing {
     sign(publishing.publications["maven"])
 }
 
-//signing {
-//    sign(publishing.publications["maven"])
-//}
-
 nexusPublishing {
     repositories {
         sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            // Correct URLs for Central Portal OSSRH Staging API
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+            username.set(findProperty("centralPortalUsername") as String? ?: System.getenv("CENTRAL_PORTAL_USERNAME"))
+            password.set(findProperty("centralPortalPassword") as String? ?: System.getenv("CENTRAL_PORTAL_PASSWORD"))
         }
     }
 }
-
-
-//mavenPublishing {
-//    configure(JavaLibrary(
-//        javadocJar = JavadocJar.Javadoc(),
-//        sourcesJar = true,
-//    ))
-//
-//    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
-//    signAllPublications()
-//
-//    coordinates(group.toString(), Meta.name, version.toString())
-//
-//    pom {
-//        name.set(Meta.name)
-//        description.set(Meta.desc)
-//        inceptionYear.set("2024")
-//        url.set("https://github.com/${Meta.githubRepo}")
-//        licenses {
-//            license {
-//                name.set("The Apache License, Version 2.0")
-//                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//            }
-//        }
-//        organization {
-//            name = "Transportial BV"
-//            url = "https://transportial.com"
-//        }
-//        developers {
-//            developer {
-//                id.set("thomaskolmans")
-//                name.set("Thomas Kolmans")
-//                url.set("https://github.com/thomaskolmans/")
-//            }
-//        }
-//        scm {
-//            url.set("https://github.com/${Meta.githubRepo}")
-//            connection.set("scm:git:git://github.com/${Meta.githubRepo}.git")
-//            developerConnection.set("scm:git:ssh://git@github.com/${Meta.githubRepo}.git")
-//        }
-//    }
-//}
